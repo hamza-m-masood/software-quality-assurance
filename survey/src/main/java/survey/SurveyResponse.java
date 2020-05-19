71385b03828275c2bf0b996e29f9fe39996a71bf
HamzaMasood1 <hamzamasood183@gmail.com>
2020-05-19 19:56:15 +0100

    created stub classes and methods for JUnit tests

---
+++ survey/src/main/java/survey/SurveyResponse.java
@@ -0,0 +1,20 @@
+package survey;
+
+import java.util.ArrayList;
+
+public class SurveyResponse {
+    ArrayList<String> answers;
+
+    public double getAverage() {
+        return 0;
+    }
+
+    public double getStandardDeviation() {
+        return 0;
+    }
+
+    public double getMinMaxScore() {
+        return 0;
+    }
+
+}
